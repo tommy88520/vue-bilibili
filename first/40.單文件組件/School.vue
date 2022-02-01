e0629f92e4bf99c039e83a1a9e611409c23a69f2
Tommy <86784639+tommy88520@users.noreply.github.com>
2022-02-01 11:35:23 +0800

    vue超基礎部分完成

---
+++ first/40.單文件組件/School.vue
@@ -0,0 +1,24 @@
+<template>
+      <div class="demo">
+        <h2>學校名稱： {{ schoolName }} </h2>
+        <h2>學校地址： {{ address }} </h2>
+      </div>
+</template>
+
+<script>
+  export default {
+    name: 'School',
+    data() {
+      return{
+        schoolName: "政大大學",
+        address:'木柵',
+      }
+    }
+  }
+</script>
+
+<style>
+  .demo{
+    background-color: orange;
+  }
+</style>
